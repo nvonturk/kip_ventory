296aa0e12ef2c660ae93dfd5d959aa4f1ea70580
Nick von Turkovich <nbv3@duke.edu>
2017-01-26 00:04:24 -0500

    New item model

--- kipventory/api/views.py
+++ kipventory/api/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
 
 from . import models, serializers
 
 # Create your views here.
-class ItemListView(generics.ListAPIView):
-    serializer_class = serializers.ItemSerializer
-
-    def get_queryset(self):
-        queryset = models.Item.objects.all()
-        return queryset
+class ItemListView(generics.ListCreateAPIView):
+	queryset = models.Item.objects.all()
+	serializer_class = serializers.ItemSerializer
