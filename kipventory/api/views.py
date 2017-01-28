0bedd50dff29c3ab26662ebc348f3cc62c11cd74
JOE TIMKO <jmt53@duke.edu>
2017-01-28 14:31:04 -0500

    Token auth working

--- kipventory/api/views.py
+++ kipventory/api/views.py
@@ -4,37 +4,65 @@ from rest_framework.response import Response
 
 from . import models, serializers
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login
 
+from rest_framework.authtoken.models import Token
+
+from django.shortcuts import redirect
+
+
 
 # Create your views here.
 class ItemListView(generics.ListAPIView):
+    # authentication_classes = (TokenAuthentication) #FOR TESTING
+    # permission_classes = (IsAuthenticated,) #FOR TESTING
     serializer_class = serializers.ItemSerializer
 
     def get_queryset(self):
+        # content = {
+        #     'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+        #     'auth': unicode(request.auth),  # None
+        # }
+        # return Response({"token" : "hey"})
         queryset = models.Item.objects.all()
         return queryset
 
 
 class ExampleView(APIView):
-    # authentication_classes = (SessionAuthentication, BasicAuthentication)
-    # permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication) #FOR TESTING
+    permission_classes = (AllowAny,) #FOR TESTING
 
     def get(self, request, format=None):
         # potentialuser = request
         username = request.query_params["username"]
         password = request.query_params["password"]
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            # login(request, user)
+        thisuser = authenticate(username=username, password=password)
+
+
+        if thisuser is not None:
+            #Should only be one
+
+            if Token.objects.filter(user=thisuser).count() == 1:
+                #User has a token lets create a new one
+
+                Token.objects.filter(user=thisuser).update(key=Token.generate_key(Token))
+                return Response({"token": Token.objects.get(user=thisuser).key})
+                ...
+            else:
+                token = Token.objects.create(user=thisuser)
+                return Response({"token": token})
+                ...
+
+            # login(request, user) I DON'T THINK WE NEED TO USE THIS, ITS NORMAL DJANGO
             #redirect and send token
             ...
-        else
+        else:
             #redirect to login failure
+
             ...
 
 
@@ -49,4 +77,5 @@ class ExampleView(APIView):
         # else:
         #     # Return an 'invalid login' error message.
         #     ...
-        return Response({"message": user.username})
+        token2 = Token.objects.values_list('key', flat=True).get(pk=1)
+        return Response({"message": token2})
--- kipventory/kipventory/settings.py
+++ kipventory/kipventory/settings.py
@@ -93,8 +93,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
