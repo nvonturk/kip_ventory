a1c021fb765f7147051ac52327f02a736b75611f
Nick von Turkovich <nbv3@duke.edu>
2017-01-28 18:17:53 -0500

    New request model. Edits to KUser model

--- kipventory/api/admin.py
+++ kipventory/api/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Item, Category, Tag, User
+from .models import Item, Category, Tag, KUser, Request
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Category)
-admin.site.register(User)
+admin.site.register(KUser)
 admin.site.register(Tag)
+admin.site.register(Request)
--- kipventory/api/models.py
+++ kipventory/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -21,7 +22,7 @@ class Tag(models.Model):
 class Item(models.Model):
     name        = models.CharField(max_length=100, unique=True)
     location    = models.CharField(max_length=100)
-    model_no     = models.CharField(max_length=100, unique=None)
+    model_no    = models.CharField(max_length=100)
     quantity    = models.IntegerField(default=0)
     description = models.TextField(max_length=500)
     tags        = models.ManyToManyField(Tag)
@@ -32,15 +33,34 @@ class Item(models.Model):
     def __str__(self):
         return self.name
 
-class User(User):
+class KUser(User):
 
     class Meta:
         proxy       = True
         ordering    = ('last_name',)
 
     def __str__(self):
+        return self.username
 
-        if self.last_name != "" and self.first_name != "":
-            return self.last_name + ', ' + self.first_name
-        else:
-            return "None"
+class Request(models.Model):
+    kuser               = models.ForeignKey(KUser, null=True)
+    items               = models.ManyToManyField(Item)
+    date_filed          = models.DateTimeField(default=timezone.now, editable=False)
+    description         = models.TextField(max_length=500)
+    ### Status Choices ###
+    WAITING             = 'Waiting'
+    APPROVED            = 'Approved'
+    COMPLETE            = 'Complete'
+    status_choices      = (
+        (WAITING  , 'Waiting'), 
+        (APPROVED , 'Approved'), 
+        (COMPLETE , 'Complete'),
+    )
+
+    status              = models.CharField(
+        max_length = 10,
+        choices = status_choices,
+        default = WAITING,
+        )
+    ######
+
