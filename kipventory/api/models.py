f89c6507e783c8526b1b7737a064160177ab1f72
Nick von Turkovich <nbv3@duke.edu>
2017-01-28 15:39:20 -0500

    Created proxy user model and tag model. Altered Item model with field for tags and tested ManyToMany relationship

--- kipventory/api/models.py
+++ kipventory/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -12,20 +13,34 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     class Meta:
-        ordering=('name',)
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
 
 class Item(models.Model):
-    part_no = models.CharField(max_length=100, unique=None)
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    quantity = models.IntegerField(default=0)
+    name        = models.CharField(max_length=100, unique=True)
+    location    = models.CharField(max_length=100)
+    model_no     = models.CharField(max_length=100, unique=None)
+    quantity    = models.IntegerField(default=0)
     description = models.TextField(max_length=500)
+    tags        = models.ManyToManyField(Tag)
 
     class Meta:
-        ordering = ('part_no',)
+        ordering = ('model_no',)
 
     def __str__(self):
         return self.name
+
+class User(User):
+
+    class Meta:
+        proxy       = True
+        ordering    = ('last_name',)
+
+    def __str__(self):
+
+        if self.last_name != "" and self.first_name != "":
+            return self.last_name + ', ' + self.first_name
+        else:
+            return "None"
