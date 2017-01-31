87cfc971ab4a90fc536fc602697c42eebc6e97a1
Nick von Turkovich <nbv3@duke.edu>
2017-01-31 18:05:48 -0500

    Beginning of adding items to cart functionality

--- assets/js/app/itemdetailmodal.jsx
+++ assets/js/app/itemdetailmodal.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { Button, Modal }  from 'react-bootstrap'
+import QuantityBox from './quantitybox'
 
 class ItemDetailModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      quantity:0,
     	showModal: false
     };
     this.close = this.close.bind(this);
     this.open = this.open.bind(this);
+    this.addToCart = this.addToCart.bind(this);
+    this.setQuantity = this.setQuantity.bind(this);
   }
 
   close() {
@@ -17,6 +21,14 @@ class ItemDetailModal extends Component {
 
   open() {
     this.setState({ showModal: true });
+  }
+
+  addToCart() {
+    this.setState({ showModal: false});
+  }
+
+  setQuantity(value){
+    this.setState({quantity:value});
   }
 
   render() {
@@ -39,6 +51,8 @@ class ItemDetailModal extends Component {
             <p>Model No: {this.props.item.model}</p>
           </Modal.Body>
           <Modal.Footer>
+            <Button onClick={this.addToCart}>Add to Cart</Button>
+            <QuantityBox onUserInput={this.setQuantity}/>
             <Button onClick={this.close}>Close</Button>
           </Modal.Footer>
         </Modal>
