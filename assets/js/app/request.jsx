3f7328554050ad7acadf5740c52af2c838f0408c
JOE TIMKO <jmt53@duke.edu>
2017-02-04 23:45:57 -0500

    moving requests into panels

--- assets/js/app/request.jsx
+++ assets/js/app/request.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import SimpleRequest from './simplerequest'
-import { Button } from 'react-bootstrap'
+import { Button, Panel } from 'react-bootstrap'
 
 function Request(props){
+  var html = null;
+  var constant_html = getReq(props);
+
+  if(props.request.status == "O"){
+    html = <Button bsStyle="primary" onClick={() => props.deleteRequest(props.request)} className="deleteRequestButton">Delete Request</Button>
+
+  } else{
+    html = "Hello World";
+  }
   return (<div>
-    <b>Item:</b> {props.request.item.name}  <SimpleRequest request={props.request}/>
-    <Button bsStyle="primary" onClick={() => props.deleteRequest(props.request)} className="deleteRequestButton">Delete Request</Button>
+    <Panel collapsible header={constant_html}>
+    {html}
+    </Panel>
     </div>)
+}
+
+function getReq(props){
+  return (
+    <div>
+    <b>Item:</b> {props.request.item.name}  <SimpleRequest request={props.request}/>
+    </div>
+  );
 }
 
 export default Request
