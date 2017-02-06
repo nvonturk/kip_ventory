ac51f2f87a9b461dd2eaef37cefb50aed87c163c
JOE TIMKO <jmt53@duke.edu>
2017-02-06 01:12:36 -0500

    Admin requests forms working

---
+++ assets/js/app/admin/adminrequest.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import SimpleRequest from '../simplerequest'
+import { Button, Panel, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+
+class AdminRequest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: this.props.request.quantity,
+      closed_comment: "test"
+    };
+  }
+
+  handleChange(name, e) {
+    /*
+    NOT CLEAR WHY THIS DIDN'T WORK
+    */
+  // handleChange(event) {
+    // console.log(event.target.value)
+    // console.log(event.target.name)
+    // this.setState({
+    //   [event.target.name]: event.target.value
+    // });
+    var change = {};
+    change[name] = e.target.value;
+    this.setState(change);
+  }
+
+
+  render(){
+    var html = [];
+    var constant_html = this.getReq(this.props);
+    if(this.props.request.status == "O"){
+      html.push(<Button bsStyle="primary" onClick={() => this.props.deleteRequest(props.request)} className="deleteRequestButton">Delete Request</Button>);
+      html.push(this.getForm(this.props));
+
+    }
+    else{
+      html.push(this.getInfo(this.props));
+    }
+
+    return (<div>
+      <Panel collapsible header={constant_html}>
+      {html}
+      </Panel>
+      </div>);
+  }
+
+  getReq(props){
+    return (
+      <div>
+      <b>Item:</b> {props.request.item.name}  <SimpleRequest request={props.request}/>
+      </div>
+    );
+  }
+
+  getInfo(props){
+      return (
+        <div>
+        <p><b>User Comments:</b> {props.request.open_reason} </p>
+        <p><b>Administrator:</b> {props.request.administrator.username} </p>
+        <p><b>Admin Comments:</b> {props.request.closed_comment} </p>
+        <p><b>Date Closed:</b> {props.request.date_closed.substring(0,9)} </p>
+        </div>
+      );
+  }
+
+  getForm(props){
+    return (
+      <FormGroup controlId="requestForm">
+        <ControlLabel>Quantity</ControlLabel>
+        <FormControl
+          type="number"
+          name="quantity"
+          value={this.state.quantity}
+          placeholder={this.state.quantity}
+          onChange={this.handleChange.bind(this, 'quantity')}
+        />
+        <ControlLabel>Closed Comment</ControlLabel>
+        <FormControl
+          type="text"
+          name="closed_comment"
+          value={this.state.closed_comment}
+          placeholder={this.state.closed_comment}
+          onChange={this.handleChange.bind(this,'closed_comment')}
+        />
+      </FormGroup>
+    );
+  }
+}
+
+
+export default AdminRequest
--- assets/js/app/admin/adminrequestscontainer.jsx
+++ assets/js/app/admin/adminrequestscontainer.jsx
@@ -1,42 +1,145 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import AdminRequestItem from './adminrequestitem'
-import $ from 'jquery'
+import $ from "jquery"
+import RequestSelectFilter from '../requestselectfilter'
+import AdminRequestList from './adminrequestlist'
+import { getCookie } from '../../csrf/DjangoCSRFToken'
 
-const AdminRequestsContainer = React.createClass({
+class AdminRequestsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requests:[],
+      all_requests:[],
+      options: [
+                { value: 'O', label: 'Outstanding' },
+                { value: 'A', label: 'Approved' },
+                { value: 'D', label: 'Denied' },
+                { value: 'all', label: 'All' }
+            ],
+      value: "all",
+      placeholder: "Request Types"
+    };
+    this.setRequests = this.setRequests.bind(this);
+    this.setFilter = this.setFilter.bind(this);
+    this.deleteRequest = this.deleteRequest.bind(this);
 
-  getInitialState(){
-    return ({allrequests: []})
-  },
 
-  getUserRequests(){
-    var thisObj = this
-    $.getJSON("/api/requests.json", function(data){
-      thisObj.setState({allrequests: data})
+
+    this.getMyRequests();
+  }
+
+  setRequests(requests){
+    this.setState({
+      requests: requests
     });
-  },
+  }
 
-  componentWillMount(){
-    this.getUserRequests()
-  },
+  setAllRequests(requests){
+    this.setState({
+      all_requests: requests
+    });
+  }
+
+  deleteRequest(request){
+    var thisobj = this
+    $.ajax({
+    url:"/api/requests/" + request.id,
+    type: "DELETE",
+    beforeSend: function(request) {
+      request.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    },
+    success:function(response){},
+    complete:function(){      var newrequests = thisobj.state.requests.filter(req => (req.id != request.id))
+          console.log("DELETED SUCCESSFULLY")
+          console.log(newrequests)
+          thisobj.setState({
+            requests: newrequests
+          })
+        },
+    error:function (xhr, textStatus, thrownError){
+        alert("error doing something");
+        console.log(xhr)
+        console.log(textStatus)
+        console.log(thrownError)
+    }
+    });
+
+  }
+
+  setFilter(type){
+    this.setState({
+      value : type.value,
+      requests: this.filterRequests(type.value)
+    });
+  }
 
 
+  getMyRequests(){
+    var thisobj = this;
+    $.getJSON("/api/requests.json", function(data){
+      thisobj.setAllRequests(data);
+      thisobj.setRequests(data);
+    });
+  }
+
+  filterRequests(option){
+    var new_reqs;
+    if(option == "all"){
+        new_reqs = this.state.all_requests.slice();
+    } else{
+        new_reqs = this.state.all_requests.filter(function(request){
+          return option == request.status;
+        });
+    }
+    return new_reqs;
+  }
 
   render() {
-    // Need to return in a window all of the requests
-
     return (
-      <Row>
-            <Col xs={10} xsOffset={1}>
-            {this.state.allrequests.map(function(request, i) {
-              return (
-                <Row key = {i}>
-                  <AdminRequestItem  request={request}/>
-                </Row>
-              )})}
-            </Col>
-      </Row>)
+      <div>
+        <RequestSelectFilter value={this.state.value} placeholder={this.state.placeholder} options={this.state.options} onChange={this.setFilter} />
+        <AdminRequestList deleteRequest={this.deleteRequest} requests={this.state.requests} />
+      </div>
+    );
   }
-})
+}
+
+// })
+
+//   getInitialState(){
+//     return ({allrequests: []})
+//   },
+//
+//   getUserRequests(){
+//     var thisObj = this
+//     $.getJSON("/api/requests.json", function(data){
+//       thisObj.setState({allrequests: data})
+//     });
+//   },
+//
+//   componentWillMount(){
+//     this.getUserRequests()
+//   },
+//
+//
+//
+//   render() {
+//     // Need to return in a window all of the requests
+//
+//     return (
+//       <Row>
+//             <Col xs={10} xsOffset={1}>
+//             {this.state.allrequests.map(function(request, i) {
+//               return (
+//                 <Row key = {i}>
+//                   <AdminRequestItem  request={request}/>
+//                 </Row>
+//               )})}
+//             </Col>
+//       </Row>)
+//   }
+// })
 
 export default AdminRequestsContainer
