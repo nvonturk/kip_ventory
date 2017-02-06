567de12ad1555f28e3a0e18de3660d0c43113f43
Jeremy Schreck <jes85@duke.edu>
2017-02-06 01:45:06 -0500

    Implemented a profile view

--- assets/js/app/profile.jsx
+++ assets/js/app/profile.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
-import AdminRequestsContainer from './admin/adminrequestscontainer'
+import $ from 'jquery'
 
-const Profile = React.createClass({
-  render() {
-    // Need to return in a window all of the requests
-    return <div>Profile</div>
+class Profile extends React.Component {
+
+  constructor(props) {
+  	super(props);
+  	this.user = this.props.route.user;
   }
-})
+
+  render() {
+  	var element = "";
+  	if(this.user) {
+  		element = (
+  			<div>
+  			<p>First name: {this.user.first_name}</p>
+    		<p>Last name: {this.user.last_name}</p>
+    		<p>Email: {this.user.email}</p>
+    		<p>Admin? {this.user.is_staff ? "Yes": "No"}</p>
+    		</div>
+    	);
+  	}
+    return (
+    	<div>
+    		{element}
+    	</div>
+    )
+  }
+}
 
 export default Profile
