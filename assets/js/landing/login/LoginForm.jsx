c464e3012b3a08d0fabf666df9142ae76dbd37c5
JOE TIMKO <jmt53@duke.edu>
2017-02-18 22:00:43 -0500

    netid redirect working

--- assets/js/landing/login/LoginForm.jsx
+++ assets/js/landing/login/LoginForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Form, FormGroup, Col, FormControl, Checkbox, Button, ControlLabel, } from 'react-bootstrap'
+// import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
 
 import { CSRFToken } from '../../csrf/DjangoCSRFToken'
 
@@ -21,26 +23,46 @@ const LoginForm = React.createClass({
     });
   },
 
+
   render() {
+    var scope = "scope=basic identity:netid:read";
+    var identity = "client_id=kipventory";
+    var redirect = "redirect_uri=https://colab-sbx-277.oit.duke.edu/app/"
+    var netid_url = "https://oauth.oit.duke.edu/oauth/authorize.php?response_type=code"+"&"+identity+"&"+redirect+"&"+scope;
+
+    console.log(netid_url);
+
     return (
-      <form method="post" action="/api/login/">
-        <CSRFToken />
-        <FormGroup controlId="username">
-          <ControlLabel>Username</ControlLabel>
-          <FormControl type="text" value={this.state.username} name="username" placeholder="Username" onChange={this.handleChange} />
-        </FormGroup>
+      <div>
+        <form method="post" action="/api/login/">
+          <CSRFToken />
+          <FormGroup controlId="username">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl type="text" value={this.state.username} name="username" placeholder="Username" onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup controlId="password">
-          <ControlLabel>Password</ControlLabel>
-          <FormControl type="password" value={this.state.password} name="password" placeholder="Password" onChange={this.handleChange} />
-        </FormGroup>
+          <FormGroup controlId="password">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl type="password" value={this.state.password} name="password" placeholder="Password" onChange={this.handleChange} />
+          </FormGroup>
 
-        <FormGroup>
-          <Button block type="submit">
-            Sign in
-          </Button>
-        </FormGroup>
-      </form>
+          <FormGroup>
+            <Button block type="submit">
+              Sign in
+            </Button>
+          </FormGroup>
+        </form>
+
+
+        <form>
+          <CSRFToken />
+          <FormGroup controlId="netid">
+            <Button block>
+              <a href={netid_url}> Login with Duke NetID</a>
+            </Button>
+          </FormGroup>
+        </form>
+      </div>
     );
   }
 });
