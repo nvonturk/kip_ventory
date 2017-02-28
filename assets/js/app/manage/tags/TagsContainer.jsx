f260e4ff9dadd9dd73531fc071be0a9bc3878c21
JOE TIMKO <jmt53@duke.edu>
2017-02-27 19:40:34 -0500

    Tags creation deletion done besides errors

--- assets/js/app/manage/tags/TagsContainer.jsx
+++ assets/js/app/manage/tags/TagsContainer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Button, Col, ListGroup, ListGroupItem } from 'react-bootstrap'
+import { Grid, Button, Col, ListGroup, ListGroupItem, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
 import $ from "jquery"
 import { getJSON, ajax } from 'jquery'
 import { getCookie } from '../../../csrf/DjangoCSRFToken'
@@ -9,6 +9,7 @@ class TagsContainer extends Component{
     super(props);
     this.state = {
       tags: [],
+      name: "",
     }
 
     this.getAllTags = this.getAllTags.bind(this);
@@ -32,8 +33,44 @@ class TagsContainer extends Component{
 
   }
 
-  createTag(event, tag){
+  handleChange(name, e) {
+    var change = {};
+    change[name] = e.target.value;
+    this.setState(change);
+  }
+
+  createTag(event){
     event.preventDefault();
+    var _this = this
+
+    if(this.state.name == ""){
+      //TODO show error here
+      console.log("Can't create blank tag")
+    } else{
+      $.ajax({
+        url:"/api/tags/",
+        type: "POST",
+        beforeSend: function(request) {
+          request.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        },
+        data: {
+          name: this.state.name,
+        },
+        traditional: true,
+        success:function(response){
+          _this.getAllTags();
+        },
+        complete:function(){
+
+        },
+        error:function (xhr, textStatus, thrownError){
+          console.log(xhr)
+          console.log(textStatus)
+          console.log(thrownError)
+          alert("error doing something");
+        }
+      });
+    }
 
   }
 
@@ -79,9 +116,19 @@ class TagsContainer extends Component{
     return (
       <div>
         <ListGroup>
-        Hello World
           {tagList}
         </ListGroup>
+        <FormGroup controlId="newTagForm">
+          <ControlLabel>New Tag:</ControlLabel>
+          <FormControl
+            type="text"
+            name="name"
+            value={this.state.name ? this.state.name : ""}
+            placeholder={this.state.name}
+            onChange={this.handleChange.bind(this, 'name')}
+          ></FormControl>
+          <Button onClick={e =>{this.createTag(e)}}>Create Tag</Button>
+        </FormGroup>
       </div>
     );
   }
