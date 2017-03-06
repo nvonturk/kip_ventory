6efdaeb14407a49623debc1dbc4a295be3b0c8f2
Brody Kellish <bd.kellish@gmail.com>
2017-03-06 15:41:54 -0500

    Fixed database reset script to work with postgres

--- resetdb.sh
+++ resetdb.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-if [ ! -f ./kipventory/db.sqlite3 ]; then
-    echo "Error - navigate to the root of the repository"
+# remove old db.sqlite3 file if it still exists
+if [ -f ./kipventory/db.sqlite3 ]; then
+    rm ./kipventory/db.sqlite3
 fi
 
-rm ./kipventory/db.sqlite3
+# remove all migrations
 rm -r ./kipventory/api/migrations/0*
 rm -r ./kipventory/api/migrations/__pycache__/
 
+# reset the database, make new migrations, and configure superuser
+python kipventory/manage.py flush --noinput
 python kipventory/manage.py makemigrations
 python kipventory/manage.py migrate
 python kipventory/manage.py createsuperuser
