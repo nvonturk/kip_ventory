3929c017801d2583ab4a23d1e05c78d3c9b3ad5c
JOE TIMKO <jmt53@duke.edu>
2017-03-20 12:07:59 -0400

    working again

--- kipventory/kipventory/crons.py
+++ kipventory/kipventory/crons.py
@@ -2,7 +2,7 @@ import os
 from django.core import management
 from django.conf import settings
 from django_cron import CronJobBase, Schedule
-
+from django.core.mail import send_mail, mail_admins
 
 class Backup(CronJobBase):
     RUN_AT_TIMES = ['06:00']
