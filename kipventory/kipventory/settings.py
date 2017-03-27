35500737920db637d4c6a82b9d7cdf22688ae8ec
Jeremy Schreck <jes85@duke.edu>
2017-03-27 01:33:19 -0400

    Email modifications

--- kipventory/kipventory/settings.py
+++ kipventory/kipventory/settings.py
@@ -177,11 +177,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # CELERY STUFF
 CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//' #rabbitMQ
-#CELERY_BROKER_URL = 'redis://localhost:6379' #redis
-'''
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
-'''
